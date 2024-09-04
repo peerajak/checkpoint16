3a07a19cc63f848af5be097b28c1fa15134ad8ef
peerajak <peerajak@gmail.com>
2024-09-04 08:27:35 +0000

    add linear algebra least square

---
+++ kinematic_model/src/kinematic_model.cpp
@@ -0,0 +1,74 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/float32_multi_array.hpp"
+//#include "NumCpp.hpp"
+#include <Eigen/Dense>
+ 
+using Eigen::MatrixXd;
+using std::placeholders::_1;
+/*
+# half of the wheel base distance
+l = 0.500/2
+# the radius of the wheels
+r = 0.254/2
+# half of track width
+w = 0.548/2
+
+    u_array = np.array(u).reshape((4, 1))
+    H = np.array([[-l-w, 1, -1],
+                  [ l+w, 1,  1],
+                  [ l+w, 1, -1],
+                  [-l-w, 1,  1]]) / r   
+
+*/
+class KinematicModel : public rclcpp::Node
+{
+public:
+  KinematicModel()
+  : Node("kinematic_model")
+  {
+
+    subscription_ = this->create_subscription<std_msgs::msg::Float32MultiArray>(
+      "wheel_speed", 10, std::bind(&KinematicModel::topic_callback, this, _1));
+  }
+
+private:
+  void topic_callback(const std_msgs::msg::Float32MultiArray::SharedPtr msg)
+  {
+    MatrixXd u(4,1);
+    
+     RCLCPP_INFO(this->get_logger(), "topic callback");
+    for(unsigned int i = 0; i < msg->data.size(); i++){
+    u(i,0) = msg->data[i];
+    RCLCPP_INFO(this->get_logger(), "I heard: '%f'",u(i,0));
+    }
+    MatrixXd twist = KinematicLeastSquareNormalEq(u);
+    RCLCPP_INFO(this->get_logger(), "twist x: '%f'",twist(0,0));
+    RCLCPP_INFO(this->get_logger(), "twist y: '%f'",twist(1,0));
+    RCLCPP_INFO(this->get_logger(), "twist w: '%f'",twist(2,0));
+  }
+
+MatrixXd KinematicLeastSquareNormalEq(MatrixXd & u){
+    MatrixXd H(4,3);
+    H(0,0) = (-l-w)/r; H(0,1) =  1/r; H(0,2) = -1/r;
+    H(1,0) = (l+w)/r;  H(1,1) =  1/r; H(1,2) =  1/r;
+    H(2,0) = (l+w)/r;  H(2,1) =  1/r; H(2,2) = -1/r;
+    H(3,0) = (-l-w)/r; H(3,1) =  1/r; H(3,2) =  1/r;
+    MatrixXd HTH_inv = (H.transpose()* H).inverse();
+    MatrixXd HTHinv_least_square  = HTH_inv * H.transpose();
+    MatrixXd twist = HTHinv_least_square * u;
+    return twist;
+}
+  
+  rclcpp::Subscription<std_msgs::msg::Float32MultiArray>::SharedPtr subscription_;
+  double l = 0.500/2;
+  double r = 0.254/2;
+  double w = 0.548/2;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<KinematicModel>());
+  rclcpp::shutdown();
+  return 0;
+}
--- wheel_velocities_publisher/src/wheel_velocities_publisher.cpp
+++ wheel_velocities_publisher/src/wheel_velocities_publisher.cpp
@@ -5,65 +5,63 @@
 
 using namespace std::chrono_literals;
 
-enum nodeState { 
-move_forward,
-move_backward,
-move_sideways_left,
-move_sideways_right,
-turn_clockwise,
-turn_counterclockwise,
-stop } nstate;
+enum nodeState {
+  move_forward,
+  move_backward,
+  move_sideways_left,
+  move_sideways_right,
+  turn_clockwise,
+  turn_counterclockwise,
+  stop
+} nstate;
 
 struct nstate_values {
-    float u[4];
-    std::string nstate_string;
+  float u[4];
+  std::string nstate_string;
 };
 
 struct nstate_values state_values[] = {
-    {{1.0, 1.0, 1.0, 1.0},"Move forward"},
-    {{-1.0, -1.0, -1.0, -1.0},"Move backward"},
-    {{-1.0, 1.0, -1.0, 1.0},"Move left"},
-    {{1.0, -1.0, 1.0, -1.0},"Move right"},
-    {{1.0, -1.0, -1.0, 1.0},"Turn clockwise"},
-    {{-1.0, 1.0, 1.0, -1.0},"Turn counter-clockwise"},
-    {{0.0, 0.0, 0.0, 0.0},"Stop"}   
-};
+    {{1.0, 1.0, 1.0, 1.0}, "Move forward"},
+    {{-1.0, -1.0, -1.0, -1.0}, "Move backward"},
+    {{-1.0, 1.0, -1.0, 1.0}, "Move left"},
+    {{1.0, -1.0, 1.0, -1.0}, "Move right"},
+    {{1.0, -1.0, -1.0, 1.0}, "Turn clockwise"},
+    {{-1.0, 1.0, 1.0, -1.0}, "Turn counter-clockwise"},
+    {{0.0, 0.0, 0.0, 0.0}, "Stop"}};
 
-class MoveRobotWheels : public rclcpp::Node
-{
+class MoveRobotWheels : public rclcpp::Node {
 public:
-  MoveRobotWheels()
-  : Node("move_robot")
-  {
-    publisher_ = this->create_publisher<std_msgs::msg::Float32MultiArray>("wheel_speed", 10);    
+  MoveRobotWheels() : Node("move_robot_wheels") {
+    publisher_ = this->create_publisher<std_msgs::msg::Float32MultiArray>(
+        "wheel_speed", 10);
     timer_1_ = this->create_wall_timer(
-      500ms, std::bind(&MoveRobotWheels::timer1_callback, this));
+        500ms, std::bind(&MoveRobotWheels::timer1_callback, this));
   }
 
 private:
-  void timer1_callback()
-  {
+  void timer1_callback() {
     auto message = std_msgs::msg::Float32MultiArray();
 
-    if(timer1_counter < 6) {
-        nstate = move_forward;
-    } else if(timer1_counter >= 6 && timer1_counter < 12){
-        nstate = move_backward;
-    } else if(timer1_counter >= 12 && timer1_counter < 18){
-        nstate = move_sideways_left;
-    } else if(timer1_counter >= 18 && timer1_counter < 24){
-        nstate = move_sideways_right;
-    } else if(timer1_counter >= 24 && timer1_counter < 30){
-        nstate = turn_clockwise;
-    } else if(timer1_counter >= 30 && timer1_counter < 36){
-        nstate = turn_counterclockwise;
-    } else if(timer1_counter >= 36 && timer1_counter < 42){
-        nstate = stop;
+    if (timer1_counter < 6) {
+      nstate = move_forward;
+    } else if (timer1_counter >= 6 && timer1_counter < 12) {
+      nstate = move_backward;
+    } else if (timer1_counter >= 12 && timer1_counter < 18) {
+      nstate = move_sideways_left;
+    } else if (timer1_counter >= 18 && timer1_counter < 24) {
+      nstate = move_sideways_right;
+    } else if (timer1_counter >= 24 && timer1_counter < 30) {
+      nstate = turn_clockwise;
+    } else if (timer1_counter >= 30 && timer1_counter < 36) {
+      nstate = turn_counterclockwise;
+    } else if (timer1_counter >= 36 && timer1_counter < 42) {
+      nstate = stop;
     } else {
       timer_1_->cancel();
     }
     RCLCPP_INFO(get_logger(), state_values[nstate].nstate_string.c_str());
-    std::vector<float> u_vector(std::begin(state_values[nstate].u), std::end(state_values[nstate].u));
+    std::vector<float> u_vector(std::begin(state_values[nstate].u),
+                                std::end(state_values[nstate].u));
     message.data = u_vector;
     publisher_->publish(message);
     timer1_counter++;
@@ -73,11 +71,9 @@ private:
 
   rclcpp::TimerBase::SharedPtr timer_1_;
   rclcpp::Publisher<std_msgs::msg::Float32MultiArray>::SharedPtr publisher_;
-
 };
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MoveRobotWheels>());
   rclcpp::shutdown();
